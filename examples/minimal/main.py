b4bc8ca53695d2c619819ba560bc91686c97ebe3
xirhxq <xirhxq@gmail.com>
2025-08-11 16:11:20 +0800

    refactor(minimal): reorganize imports and update drawing logic
    - Replace 'from mr_dapa.drawers.drawers import *' with explicit imports
    - Update drawing logic to use the imported modules
    - Improve code readability with line breaks and consistent formatting
    - Add version number and import necessary components in mr_dapa/__init__.py

--- examples/minimal/main.py
+++ examples/minimal/main.py
@@ -1,38 +1,38 @@
-from mr_dapa.drawers.drawers import *
+import mr_dapa as mrdp
 
 components = {
-    'x': { # basic component for drawing X position
-        'title': 'X Position', # subplot title
-        'class': 'LinesComponent', # component to use
-        'keys': ['x'], # keys to plot
+    'x': {  # basic component for drawing X position
+        'title': 'X Position',  # subplot title
+        'class': 'LinesComponent',  # component to use
+        'keys': ['x'],  # keys to plot
     },
     'x-with-bars': {
         'title': 'X Position',
         'class': 'LinesComponent',
         'keys': ['x'],
-        'bars': [-1, 0, 1], # customised horizontal lines
+        'bars': [-1, 0, 1],  # customised horizontal lines
     },
     'y': {
         'title': 'Y Position',
         'class': 'LinesComponent',
         'keys': ['y'],
-        'filename': 'ypos', # customised saving filename
+        'filename': 'ypos',  # customised saving filename
     },
     'y-with-range': {
         'title': 'Y Position',
         'class': 'LinesComponent',
         'keys': ['y'],
-        'range': [-0.5, 3.9], # customised grey area
+        'range': [-0.5, 3.9],  # customised grey area
     },
     'xy-line': {
         'title': 'X & Y Position, Separated',
         'class': 'LinesComponent',
         'keys': ['x', 'y'],
-        'figsize': (12, 8), # customised figure size
+        'figsize': (12, 8),  # customised figure size
     },
     'map': {
         'title': 'Map',
-        'class': 'MapComponent', # 2d map component
+        'class': 'MapComponent',  # 2d map component
     }
 }
 
@@ -41,52 +41,77 @@ def minimal_main():
     file = 'data.json'
 
     # plot all x and all y in two subplots
-    StaticGlobalPlotDrawer([file]).register_components(components).draw(['x', 'y'])
+    mrdp.StaticGlobalPlotDrawer([file]).register_components(components) \
+        .draw(['x', 'y'])
 
     # plot all x and all y with customised bars and range
-    StaticGlobalPlotDrawer([file]).register_components(components).draw(['x-with-bars', 'y-with-range'])
+    mrdp.StaticGlobalPlotDrawer([file]).register_components(components) \
+        .draw(['x-with-bars', 'y-with-range'])
 
     # we can specify which robots to plot
-    StaticGlobalPlotDrawer([file]).register_components(components).set_id_list([2, 3]).draw(['xy-line'])
+    mrdp.StaticGlobalPlotDrawer([file]).register_components(components) \
+        .set_id_list([2, 3]) \
+        .draw(['xy-line'])
 
     # we can also specify time range
-    StaticGlobalPlotDrawer([file]).register_components(components).set_time_range((0.2, 0.5)).draw(['x', 'y'])
+    mrdp.StaticGlobalPlotDrawer([file]).register_components(components) \
+        .set_time_range((0.2, 0.5)) \
+        .draw(['x', 'y'])
 
     # also specify last seconds
-    StaticGlobalPlotDrawer([file]).register_components(components).set_last_seconds(2).draw(['x', 'y'])
+    mrdp.StaticGlobalPlotDrawer([file]).register_components(components) \
+        .set_last_seconds(2).draw(['x', 'y'])
 
     # plot x position per robot in separate plots
-    StaticSeparatePlotDrawer([file]).register_components(components).draw(['x'])
+    mrdp.StaticSeparatePlotDrawer([file]).register_components(components) \
+        .draw(['x'])
 
     # this mode also support customising time range
-    StaticSeparatePlotDrawer([file]).register_components(components).set_first_seconds(2).draw(['x', 'y'])
+    mrdp.StaticSeparatePlotDrawer([file]).register_components(components) \
+        .set_first_seconds(2) \
+        .draw(['x', 'y'])
 
     # plot all x and all y per robot, but in one plot
-    StaticGroupPlotDrawer([file]).register_components(components).draw(['x', 'y'])
+    mrdp.StaticGroupPlotDrawer([file]).register_components(components) \
+        .draw(['x', 'y'])
 
     # set time range and id list at the same time
-    StaticGroupPlotDrawer([file]).register_components(components).set_time_range((0.2, 0.5)).set_id_list([1, 3]).draw(['x', 'y'])
+    mrdp.StaticGroupPlotDrawer([file]).register_components(components) \
+        .set_time_range((0.2, 0.5)) \
+        .set_id_list([1, 3]) \
+        .draw(['x', 'y'])
 
     # plot animation
-    AnimationDrawer([file]).register_components(components).draw(['x', 'y'])
+    mrdp.AnimationDrawer([file]).register_components(components) \
+        .draw(['x', 'y'])
 
     # speed up 2x
-    AnimationDrawer([file]).register_components(components).draw(['x', 'y'], time_ratio=2)
+    mrdp.AnimationDrawer([file]).register_components(components) \
+        .draw(['x', 'y'], time_ratio=2)
 
     # customising time range
-    AnimationDrawer([file]).register_components(components).set_time_range((0.2, 0.5)).draw(['x'])
+    mrdp.AnimationDrawer([file]).register_components(components) \
+        .set_time_range((0.2, 0.5)).draw(['x'])
 
     # customising last seconds, then specify fps
-    AnimationDrawer([file]).register_components(components).set_last_seconds(2).draw(['y'], fps=20)
+    mrdp.AnimationDrawer([file]).register_components(components) \
+        .set_last_seconds(2).draw(['y'], fps=20)
 
     # plot 2d map
-    AnimationDrawer([file]).register_components(components).draw(['map'])
+    mrdp.AnimationDrawer([file]).register_components(components) \
+        .draw(['map'])
 
     # we could draw animation with multiple components
-    AnimationDrawer([file]).register_components(components).set_time_range((0.2, 0.5)).draw(['map', 'xy-line'])
+    mrdp.AnimationDrawer([file]).register_components(components) \
+        .set_time_range((0.2, 0.5)) \
+        .draw(['map', 'xy-line'])
 
     # we can also specify id list
-    AnimationDrawer([file]).register_components(components).set_last_seconds(2).set_id_list([1, 3]).draw(['map', 'x', 'y'])
+    mrdp.AnimationDrawer([file]).register_components(components) \
+        .set_last_seconds(2) \
+        .set_id_list([1, 3]). \
+        draw(['map', 'x', 'y'])
+
 
 if __name__ == '__main__':
     minimal_main()
---
+++ mr_dapa/__init__.py
@@ -0,0 +1,13 @@
+from .drawers.drawers import (
+    StaticGlobalPlotDrawer,
+    StaticSeparatePlotDrawer,
+    StaticGroupPlotDrawer,
+    AnimationDrawer,
+)
+
+from .components.components import (
+    LinesComponent,
+    MapComponent
+)
+
+__version__ = '0.1'
