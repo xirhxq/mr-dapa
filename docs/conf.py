b2e7c51ccef9c9782c3bb959af2472c2f831f1b7
xirhxq <373836275@qq.com>
2026-03-31 14:03:52 +0800

    fix(docs): update version to 0.5.0 in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'mr-dapa'
 copyright = '2025, mr-dapa contributors'
 author = 'mr-dapa contributors'
-release = '0.4.0'
+release = '0.5.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
